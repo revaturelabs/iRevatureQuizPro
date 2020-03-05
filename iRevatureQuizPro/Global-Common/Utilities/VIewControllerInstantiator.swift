e9754348e5ec4a19e075fb97ac5e2e84946f530c
Mark Hawkins <markhawkins@USF-Gold-Wifi-226-118-43.laptops.usf.edu>
2020-03-05 12:22:22 -0500

    Added Deployment diagram; Added easy viewcontroller instantiation

---
+++ iRevatureQuizPro/Global-Common/Utilities/VIewControllerInstantiator.swift
@@ -0,0 +1,52 @@
+//
+//  VIewControllerInstantiator.swift
+//  iRevatureTrainingRoomMaintenanceScheduler
+//
+//  Created by Mark Hawkins on 2/12/20.
+//  Copyright © 2020 revature. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//Add Stoyboard Names
+enum AppStoryboard: String {
+    case Main
+}
+
+extension AppStoryboard {
+    //Create an instance of UIStoryboard
+    var instance : UIStoryboard {
+        return UIStoryboard(name: self.rawValue, bundle: Bundle.main)
+    }
+    
+    //Create an instance of View Controller
+    func viewController<T : UIViewController>(viewControllerClass : T.Type) -> T {
+        let storyboardID = (viewControllerClass as UIViewController.Type).storyboardID
+         
+        //Make sure that the view controller is properly instantiated
+        guard let controller = instance.instantiateViewController(withIdentifier: storyboardID) as? T else {
+            fatalError("ViewController: \(storyboardID), not found in \(self.rawValue).")
+        }
+        
+        return controller
+    }
+    
+    //Set initial view controller
+    func initialViewController() -> UIViewController? {
+        return instance.instantiateInitialViewController()
+    }
+}
+
+extension UIViewController {
+    //Get the storyboard id of the current view controller
+    //This holds to the convention of <ViewControllerClassName>_ID
+    class var storyboardID : String {
+        return "\(self)_ID"
+    }
+    
+    //Create an instance of view controller
+    static func instantiate(fromAppStoryboard appStoryboard: AppStoryboard) -> Self {
+        return appStoryboard.viewController(viewControllerClass: self)
+    }
+}
