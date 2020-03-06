4adeb5e1c2563e8decc462e3ef70303663ed7821
Mark Hawkins <markhawkins@USF-Gold-Wifi-226-118-43.laptops.usf.edu>
2020-03-06 14:09:33 -0500

    added persistence table protocol

--- iRevatureQuizPro/Persistence/Entities/EventAttendee/EventAttendeeTable.swift
+++ iRevatureQuizPro/Persistence/Entities/EventAttendee/EventAttendeeTable.swift
@@ -1,8 +1,8 @@
 //
-//  MetatagTable.swift
+//  EventAttendeeTable.swift
 //  iRevatureQuizPro
 //
-//  Created by Mark Hawkins on 3/5/20.
+//  Created by Mark Hawkins on 3/6/20.
 //  Copyright © 2020 revature. All rights reserved.
 //
 
@@ -11,21 +11,27 @@ import EasySQLite
 //=====================================
     //Metatag Table information
 //=====================================
-struct PMetatag: PersistenceTable {
+struct PEventAttendee: PersistenceTable {
     //Table's name
-    static let tableName = "metatag"
+    static let tableName = "event_attendee"
     
     //Table's Column Names
     enum ColumnName: String {
         case id = "tag_id"
-        case name = "tag_name"
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case email = "email"
+        case phone = "phone"
+        case major = "major"
+        case highestEduc = "highest_enducation"
+        case workAuth = "work_authorization"
     }
     
     //Table
     static var table: ESLTable {
         var table = ESLTable(tableName: tableName)
         table.addColumn(columnName: ColumnName.id.rawValue, dataType: .INTEGER, constraints: .PRIMARYKEY, .AUTOINCREMENT)
-        table.addColumn(columnName: ColumnName.name.rawValue, dataType: .CHAR, constraints: .UNIQUE, .NOTNULL)
+        //table.addColumn(columnName: ColumnName.name.rawValue, dataType: .CHAR, constraints: .UNIQUE, .NOTNULL)
         
         return table
     }
