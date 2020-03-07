cc708571b944f37eb5e66778e600361524f36299
Mark Hawkins <markhawkins@Marks-MacBook-Pro.local>
2020-03-07 01:02:08 -0500

    Reworked attendee signup with auto-layout; added extension to remove keyboard when tapped off of it

--- iRevatureQuizPro/Application/Modules/View-Controllers/AttendeeSignUpViewController.swift
+++ iRevatureQuizPro/Application/Modules/View-Controllers/AttendeeSignUpViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class AttendeeSignUpViewController: BaseViewController {
 
+    @IBOutlet weak var textTest: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
