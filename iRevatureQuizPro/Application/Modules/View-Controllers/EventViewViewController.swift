0528558e007196388abf6c8271632d8764775b41
Nathan Coggins <nthndcoggins@gmail.com>
2020-03-09 16:23:05 -0400

    Gets events for array and calculate size

--- iRevatureQuizPro/Application/Modules/View-Controllers/EventViewViewController.swift
+++ iRevatureQuizPro/Application/Modules/View-Controllers/EventViewViewController.swift
@@ -7,17 +7,19 @@
 //
 
 import UIKit
+import EasySQLite
 
 class EventViewViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return eventsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         <#code#>
     }
     
-
+    var eventsArray:[Events.EventsAllData] = []
+    
     @IBOutlet weak var eventSearchBar: UISearchBar!
     @IBOutlet weak var eventsTable: UITableView!
     
@@ -25,9 +27,9 @@ class EventViewViewController: BaseViewController, UITableViewDelegate, UITableV
         super.viewDidLoad()
         self.eventsTable.dataSource = self
         self.eventsTable.delegate = self
-        // Do any additional setup after loading the view.
+        self.eventsArray = initArray()
     }
-
+    
     @IBAction func addEventButton(_ sender: Any) {
         let nextVC = CreateEventViewController.instantiate(fromAppStoryboard: AppStoryboard.EventManagement)
         nextVC.modalPresentationStyle = .fullScreen
@@ -38,5 +40,18 @@ class EventViewViewController: BaseViewController, UITableViewDelegate, UITableV
         let nextVC = EmployeeLoginViewController.instantiate(fromAppStoryboard: AppStoryboard.EmployeeLogin)
         nextVC.modalPresentationStyle = .fullScreen
         present(nextVC, animated: false, completion: nil)
+    }
+    
+    func initArray() -> [Events.EventsAllData]{
+        var statement = ESLSelectStatement()
+        statement.specifyColumn(table: Events.table, columnName: Events.ColumnName.quizID.rawValue, asName: Events.ColumnName.quizID.rawValue)
+        statement.specifyColumn(table: Events.table, columnName: Events.ColumnName.ambassadorEmail.rawValue, asName: Events.ColumnName.ambassadorEmail.rawValue)
+        statement.specifyColumn(table: Events.table, columnName: Events.ColumnName.date.rawValue, asName: Events.ColumnName.date.rawValue)
+        statement.specifyColumn(table: Events.table, columnName: Events.ColumnName.eventCode.rawValue, asName: Events.ColumnName.eventCode.rawValue)
+        statement.specifyColumn(table: Events.table, columnName: Events.ColumnName.eventName.rawValue, asName: Events.ColumnName.eventName.rawValue)
+        statement.specifyColumn(table: Events.table, columnName: Events.ColumnName.location.rawValue, asName: Events.ColumnName.location.rawValue)
+        
+        return Database.selectRow(withSelectStatement: statement, returnStruct: Events.EventsAllData.self)!
+        
     }
 }
