0664699b51f18307e811ac651e242a72be9b012e
Kjay Shieu <kxs646@case.edu>
2020-03-10 11:18:54 -0400

    changed the database to Date type

--- iRevatureQuizPro/Application/Modules/View-Controllers/EventViewViewController.swift
+++ iRevatureQuizPro/Application/Modules/View-Controllers/EventViewViewController.swift
@@ -15,8 +15,11 @@ class EventViewViewController: BaseViewController, UITableViewDelegate, UITableV
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let df = DateFormatter()
+        df.dateFormat = "MMM dd, yyyy"
+        
         let cell = eventsTable.dequeueReusableCell(withIdentifier: "EventsTableCell", for: indexPath) as! EventCell
-        cell.date?.text = eventsArray[indexPath.row].date
+        cell.date?.text = df.string(from: eventsArray[indexPath.row].date)
         cell.eventName?.text = eventsArray[indexPath.row].event_name
         cell.location?.text = eventsArray[indexPath.row].location
         return cell
