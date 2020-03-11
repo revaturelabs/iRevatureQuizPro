3c91806c86aeedb3c342b1746895097411fed188
Nathan Coggins <nthndcoggins@gmail.com>
2020-03-11 12:23:48 -0400

    Created Structs for Questions
    Co-Authored-By: sbaftermath <sbaftermath@users.noreply.github.com>

---
+++ iRevatureQuizPro/Business/Entities/QuestionObject.swift
@@ -0,0 +1,16 @@
+//
+//  QuestionObject.swift
+//  iRevatureQuizPro
+//
+//  Created by Nathan Coggins on 3/11/20.
+//  Copyright © 2020 revature. All rights reserved.
+//
+
+import Foundation
+
+struct questionObject{
+    var id: Int
+    var title: String
+    var tags: String
+    var questionType: String
+}
---
+++ iRevatureQuizPro/Business/Services/QuestionsParsing/QuestionsParsingBusinessService.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionsParsing.swift
+//  iRevatureQuizPro
+//
+//  Created by Kjay Shieu on 3/11/20.
+//  Copyright © 2020 revature. All rights reserved.
+//
+
+import Foundation
+
+class QuestionsParsingBusinessService{
+    
+}
