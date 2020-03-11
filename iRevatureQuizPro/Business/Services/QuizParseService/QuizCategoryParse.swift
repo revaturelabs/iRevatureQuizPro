2f00e0595e52ce2c171e50ba720c087a233c435a
jmalisse <jeremy.malisse@gmail.com>
2020-03-11 14:28:43 -0400

    removed duplicate Parse file. Updated quick print for parse service

--- iRevatureQuizPro/Business/Services/QuizParseService/QuizCategoryParse.swift
+++ iRevatureQuizPro/Business/Services/QuizParseService/QuizCategoryParse.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+struct QuizCategoryParse {
+	
+	func loadData() {
+		QuizDataAccess.getAllQuizzes(numberOfRecords: 200, finish: {
+			results in
+			
+			print("should be results next")
+			print(results)
+			
+		})
+		
+		/* Ned to come back to this when the service is finished - cannot check quiz by ID until then
+		QuizDataAccess.getQuizById(quizId: <#T##String#>, finish: {
+			results in
+			
+		})
+		*/
+	}
+}
