fdfe993784916de8f697de0b0184ecc795b3eb53
Nathan Coggins <nthndcoggins@gmail.com>
2020-03-12 11:38:15 -0400

    Updated QuestionStatements.swift
    Co-Authored-By: Wes <wesotero@users.noreply.github.com>

--- iRevatureQuizPro/Persistence/Entities/Question/QuestionStatements.swift
+++ iRevatureQuizPro/Persistence/Entities/Question/QuestionStatements.swift
@@ -9,6 +9,7 @@
 import EasySQLite
 
 extension PQuestion {
+    
 //=====================================
     //Select All Question Rows Statement
 //=====================================
@@ -98,47 +99,18 @@ extension PQuestion {
 //=====================================
     //Update Question Row By ID Statement
 //=====================================
-    static func updateByStatement(quizobject: BQuiz) -> ESLUpdateStatement {
+    static func updateByStatement(questionObject: QuestionObject) -> ESLUpdateStatement {
         //Update Statement
-        var update = ESLUpdateStatement(table: PQuiz.table)
+        var update = ESLUpdateStatement(table: PQuestion.table)
         
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.quiz_name.rawValue,                                  updatedValue: quizobject.quiz_name)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.quiz_id.rawValue,                                    updatedValue: quizobject.quiz_id)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.activity_points.rawValue,                            updatedValue: quizobject.activity_points)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.duration.rawValue,                                   updatedValue: quizobject.duration)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.max_attempts.rawValue,                               updatedValue: quizobject.max_attempts)
+        update.addValueChange(columnToUpdate: PQuestion.ColumnName.question_name.rawValue,                                  updatedValue: questionObject.title)
+        update.addValueChange(columnToUpdate: PQuestion.ColumnName.question_type.rawValue, updatedValue: questionObject.questionType)
+        update.addValueChange(columnToUpdate: PQuestion.ColumnName.tags.rawValue, updatedValue: questionObject.tags)
         
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.level_override.rawValue,                             updatedValue: quizobject.level_override)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.slug.rawValue,                                       updatedValue: quizobject.slug)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.description.rawValue,                                updatedValue: quizobject.description)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.meta_description.rawValue,                           updatedValue: quizobject.meta_description)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.image_url.rawValue,                                  updatedValue: quizobject.image_url)
-        
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.pass_percentage.rawValue,                            updatedValue: quizobject.pass_percentage)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.via_slug.rawValue,                                   updatedValue: quizobject.via_slug)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.timer.rawValue,                                      updatedValue: quizobject.timer)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.shuffle_questions.rawValue,                          updatedValue: quizobject.shuffle_questions)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.shuffle_answers.rawValue,                            updatedValue: quizobject.shuffle_answers)
-        
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.display_score_and_result.rawValue,                   updatedValue: quizobject.display_score_and_result)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.allow_attempt_review.rawValue,                       updatedValue: quizobject.allow_attempt_review)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.show_whether_correct.rawValue,                       updatedValue: quizobject.show_whether_correct)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.show_correct_answers_passed.rawValue,                updatedValue: quizobject.show_correct_answers_passed)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.show_correct_answers_failed.rawValue,                updatedValue: quizobject.show_correct_answers_failed)
-        
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.show_answer_explanations.rawValue,                   updatedValue: quizobject.show_answer_explanations)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.save_and_resume.rawValue,                            updatedValue: quizobject.save_and_resume)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.instructions.rawValue,                               updatedValue: quizobject.instructions)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.mode.rawValue,                                       updatedValue: quizobject.mode)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.created_by.rawValue,                                 updatedValue: quizobject.created_by)
-        
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.created_on.rawValue,                                 updatedValue: quizobject.created_on)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.updated_by.rawValue,                                 updatedValue: quizobject.updated_by)
-        update.addValueChange(columnToUpdate: PQuiz.ColumnName.updated_on.rawValue,                                 updatedValue: quizobject.updated_on)
         
         //Where Statement
         var `where` = ESLWhereStatement()
-        `where`.addStatement(table: PQuiz.table, columnName: PQuiz.ColumnName.quiz_id.rawValue, expression: .EQUALS, columnValue: quizobject.quiz_id)
+        `where`.addStatement(table: PQuestion.table, columnName: PQuestion.ColumnName.question_id.rawValue, expression: .EQUALS, columnValue: questionObject.id)
         
         update.setWhereStatement(statement: `where`)
         
