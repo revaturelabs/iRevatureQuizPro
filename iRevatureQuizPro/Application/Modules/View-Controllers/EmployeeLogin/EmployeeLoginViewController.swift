227e87ff857f76012ac5ed0020d6fc0a1b58ac28
Nathan Coggins <nthndcoggins@gmail.com>
2020-03-13 10:29:14 -0400

    Toast Message Tweaks

--- iRevatureQuizPro/Application/Modules/View-Controllers/EmployeeLogin/EmployeeLoginViewController.swift
+++ iRevatureQuizPro/Application/Modules/View-Controllers/EmployeeLogin/EmployeeLoginViewController.swift
@@ -68,9 +68,9 @@ class EmployeeLoginViewController: BaseViewController {
     }
     
     func showToast(message : String) {
-        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/8, y: self.view.frame.size.height/2, width: 300, height: 50))
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/8, y: self.view.frame.size.height/1.5, width: 300, height: 50))
         toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-        toastLabel.textColor = UIColor.red
+        toastLabel.textColor = UIColor.white
         toastLabel.textAlignment = .center;
         toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
         toastLabel.text = message
@@ -78,7 +78,7 @@ class EmployeeLoginViewController: BaseViewController {
         toastLabel.layer.cornerRadius = 10;
         toastLabel.clipsToBounds  =  true
         self.view.addSubview(toastLabel)
-        UIView.animate(withDuration: 8.0, delay: 4, options: .curveEaseOut, animations: {
+        UIView.animate(withDuration: 3.0, delay: 1.5, options: .curveEaseOut, animations: {
             toastLabel.alpha = 0.0
         }, completion: {(isCompleted) in
             toastLabel.removeFromSuperview()
