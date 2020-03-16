b2be1f8f291cd46e84cf2bc32f6bd2a09cf4b011
Wes <wesleyotero@protonmail.com>
2020-03-16 09:47:23 -0400

    Adds comments to HelpViewController

--- iRevatureQuizPro/Application/Modules/View-Controllers/Help/HelpViewController.swift
+++ iRevatureQuizPro/Application/Modules/View-Controllers/Help/HelpViewController.swift
@@ -5,6 +5,9 @@
 //  Created by Wesley Otero on 3/13/20.
 //  Copyright © 2020 revature. All rights reserved.
 //
+/*
+    Allows the user to explore the revature website
+*/
 
 import WebKit
 
@@ -12,6 +15,7 @@ class HelpViewController : BaseViewController, WKNavigationDelegate{
     
     var webView: WKWebView!
     
+    // Setup for loading the WKWeb View
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
@@ -21,8 +25,10 @@ class HelpViewController : BaseViewController, WKNavigationDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // URL that the webview will display
         let url = URL(string: "https://www.revature.com/")!
         
+        //Assigns the URL to the webview
         webView.load(URLRequest(url: url))
         
         webView.allowsBackForwardNavigationGestures = true
