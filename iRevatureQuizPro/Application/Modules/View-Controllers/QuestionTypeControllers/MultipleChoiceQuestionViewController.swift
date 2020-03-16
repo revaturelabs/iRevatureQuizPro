3233ded70fd6b500f4c75ebded565a212a8e34a3
Wes <wesleyotero@protonmail.com>
2020-03-16 16:10:28 -0400

    Adds MultChoiceViewController, and QuizQuestion storyboard

---
+++ iRevatureQuizPro/Application/Modules/View-Controllers/QuestionTypeControllers/MultipleChoiceQuestionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MultipleChoiceQuestionViewController.swift
+//  iRevatureQuizPro
+//
+//  Created by Wesley Otero on 3/16/20.
+//  Copyright © 2020 revature. All rights reserved.
+//
+
+import UIKit
+
+class MultipleChoiceQuestionViewController: BaseViewController {
+    
+    @IBOutlet weak var answerTableView: UITableView!
+}
