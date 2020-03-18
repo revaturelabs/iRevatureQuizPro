b83ac227168286a0233e1aaf98a3cf8a53e53367
Kyle Keck <keckkyle@gmail.com>
2020-03-18 11:48:08 -0400

    Merge & update structs

--- iRevatureQuizPro/Business/Services/AttendeeQuizService.swift
+++ iRevatureQuizPro/Business/Services/AttendeeQuizService.swift
@@ -10,35 +10,35 @@ import Foundation
 
 class AttendeeQuizService {
 
-    private static var currentQuiz = Quiz(title: "No Quiz", questions: [], duration: 0)
+    private static var currentQuiz = TakeQuizQuiz(title: "No Quiz", questions: [], duration: 0)
 
     static func fetchCurrentQuiz(quizId: Int) {
         
         QuizAPI.getQuizById(quizId: quizId) { (result) in
             //set questions array
-            var questions = [Question]()
+            var questions = [TakeQuizQuestion]()
             //go through question pools to get data
             for quizPool in result.data.quizPools! {
                 //go through questions in questionpool
                 for question in quizPool.quizPoolQuestions {
                     //set answers array
-                    var answers = [Answer]()
+                    var answers = [TakeQuizAnswer]()
                     //go through answers in the question
                     for answer in question.question.qsnAnswers {
                         //add answers to answer array
-                        answers.append(Answer(answer: answer.answer, isCorrect: answer.correct, isSelected: false))
+                        answers.append(TakeQuizAnswer(answer: answer.answer, isCorrect: answer.correct, isSelected: false))
                     }
                     //add questions to question array
-                    questions.append(Question(question: question.question.title, answers: answers, isAnswered: false))
+                    questions.append(TakeQuizQuestion(question: question.question.title, answers: answers, isAnswered: false))
                 }
             }
             
-            self.currentQuiz = Quiz(title: result.data.title, questions: questions, duration: result.data.quizDuration)
+            self.currentQuiz = TakeQuizQuiz(title: result.data.title, questions: questions, duration: result.data.quizDuration)
         }
         
     }
     
-    static func getCurrentQuiz() -> Quiz {
+    static func getCurrentQuiz() -> TakeQuizQuiz {
         return self.currentQuiz
     }
 
