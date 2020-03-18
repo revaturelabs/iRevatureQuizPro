1f5e345b1a51942e01e97177868d8e13169ae03c
Mark Hawkins <markhawkins@Marks-MacBook-Pro.local>
2020-03-18 15:34:24 -0400

    updated EventViewViewController to populate events tab.

--- iRevatureQuizPro/Application/Modules/View-Controllers/EventManagement/EventViewViewController.swift
+++ iRevatureQuizPro/Application/Modules/View-Controllers/EventManagement/EventViewViewController.swift
@@ -9,31 +9,25 @@
 import UIKit
 import EasySQLite
 
-class EventViewViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return eventsArray.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let df = DateFormatter()
-        df.dateFormat = "MMM dd, yyyy"
-        
-        let cell = eventsTable.dequeueReusableCell(withIdentifier: "EventsTableCell", for: indexPath) as! EventCell
-        cell.date?.text = df.string(from: eventsArray[indexPath.row].date)
-        cell.eventName?.text = eventsArray[indexPath.row].event_name
-        cell.location?.text = eventsArray[indexPath.row].location
-        return cell
-    }
-    
-    var eventsArray:[Events.EventsAllData] = []
+class EventViewViewController: BaseViewController {
+    var eventsArray = [Events.EventsAllData]()
     
     @IBOutlet weak var eventSearchBar: UISearchBar!
     @IBOutlet weak var eventsTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        eventsTable.dataSource = self
+        eventsTable.delegate = self
+        
+        initArray()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        initArray()
     }
     
     @IBAction func addEventButton(_ sender: Any) {
@@ -48,7 +42,7 @@ class EventViewViewController: BaseViewController, UITableViewDelegate, UITableV
         present(nextVC, animated: false, completion: nil)
     }
     
-    func initArray() -> [Events.EventsAllData]{
+    func initArray() {
         var statement = ESLSelectStatement()
         statement.specifyColumn(table: Events.table, columnName: Events.ColumnName.quizID.rawValue, asName: Events.ColumnName.quizID.rawValue)
         statement.specifyColumn(table: Events.table, columnName: Events.ColumnName.ambassadorEmail.rawValue, asName: Events.ColumnName.ambassadorEmail.rawValue)
@@ -56,7 +50,29 @@ class EventViewViewController: BaseViewController, UITableViewDelegate, UITableV
         statement.specifyColumn(table: Events.table, columnName: Events.ColumnName.eventCode.rawValue, asName: Events.ColumnName.eventCode.rawValue)
         statement.specifyColumn(table: Events.table, columnName: Events.ColumnName.eventName.rawValue, asName: Events.ColumnName.eventName.rawValue)
         statement.specifyColumn(table: Events.table, columnName: Events.ColumnName.location.rawValue, asName: Events.ColumnName.location.rawValue)
-        return []
-//        return Database.selectRow(withSelectStatement: statement, returnStruct: Events.EventsAllData.self)!
+
+        self.eventsArray = Database.selectRow(withSelectStatement: statement, returnStruct: Events.EventsAllData.self)!
+        
+        eventsTable.reloadData()
+    }
+}
+
+extension EventViewViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(eventsArray.count)
+        return eventsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let df = DateFormatter()
+        df.dateFormat = "MMM dd, yyyy"
+        
+        let cell = eventsTable.dequeueReusableCell(withIdentifier: "EventsTableCell", for: indexPath) as! EventCell
+        cell.date?.text = df.string(from: eventsArray[indexPath.row].date)
+        cell.eventName?.text = eventsArray[indexPath.row].event_name
+        cell.location?.text = eventsArray[indexPath.row].location
+        cell.quiz.text = "Quiz ID: \(eventsArray[indexPath.row].quiz_id)"
+        print(cell)
+        return cell
     }
 }
