e7a9220fb0003a8e2c9afd24761a358d63c4de6a
Wes <wesleyotero@protonmail.com>
2020-03-18 10:59:04 -0400

    Updates to MCQuestionTableVC, MultChoiceQuestionVC, and QuestionPageVC

--- iRevatureQuizPro/Application/Modules/View-Controllers/QuestionTypeControllers/MultipleChoiceQuestionViewController.swift
+++ iRevatureQuizPro/Application/Modules/View-Controllers/QuestionTypeControllers/MultipleChoiceQuestionViewController.swift
@@ -17,16 +17,11 @@ class MultipleChoiceQuestionViewController: BaseViewController {
     @IBOutlet weak var questionTextView: UITextView!
     
     @IBOutlet weak var answerTableView: UITableView!
+    
     private let tableController = MCQuestionTableController()
     
-    let answers = [
-        QuestionAnswer(id: 123, answer: "Answer 1", explanation: "", order: 1, correct: false, sticky: false),
-        QuestionAnswer(id: 132, answer: "Answer 2", explanation: "", order: 2, correct: true, sticky: false),
-        QuestionAnswer(id:987, answer: "Some Answer", explanation: "", order: 3, correct: false, sticky: false)
-    ]
-    
     // Question object to be displayed
-    var question: Question?
+    var question: TakeQuizQuestion?
     
     var questionIndex: Int = 0
     
@@ -37,16 +32,15 @@ class MultipleChoiceQuestionViewController: BaseViewController {
 		timerLabel.setTimer(with: 45)
         
         timerLabel.runTimer()
-        
 
-        tableController.setAnswers(answers: answers)
+        tableController.setAnswers(answers: question!.answers)
 
         currentQuestionLabel.text = String("\(questionIndex + 1)")
         
         questionTextView.text = question?.question
-
         
         answerTableView.dataSource = tableController
+        
         answerTableView.delegate = tableController
     }
     
--- iRevatureQuizPro/Application/Modules/View-Controllers/QuestionTypeControllers/QuestionPageViewController.swift
+++ iRevatureQuizPro/Application/Modules/View-Controllers/QuestionTypeControllers/QuestionPageViewController.swift
@@ -15,16 +15,14 @@ class QuestionPageViewController: UIPageViewController{
     
     public var answerIndex: [String : Int] = [:]
     // List of questions
-    public var questionList: [Question]? = []
+    public var questionList: [TakeQuizQuestion]? = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        questionList?.append(Question(question: "question 1", answers: [], isAnswered: false, correctAnswer: 1))
-//
-//        questionList?.append(Question(question: "question 2", answers: [], isAnswered: false, correctAnswer: 1))
-
-            
+        questionList?.append(TakeQuizQuestion(question: "Is this working", answers: [TakeQuizAnswer(answer: "Yes", isCorrect: true, isSelected: false), TakeQuizAnswer(answer: "No", isCorrect: false, isSelected: false)], isAnswered: false))
+        
+        
         self.dataSource = self
         
         self.setViewControllers([getViewControllerAtIndex(index: questionIndex ?? 0)] as [UIViewController], direction: UIPageViewController.NavigationDirection.forward, animated: false, completion: nil)
