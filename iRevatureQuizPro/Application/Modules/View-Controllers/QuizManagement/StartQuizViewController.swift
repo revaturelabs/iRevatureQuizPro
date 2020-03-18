6fadc4683e20c136d0064666deed68fa088302ed
Wes <wesleyotero@protonmail.com>
2020-03-18 14:43:14 -0400

    Update to StartQuizVC

--- iRevatureQuizPro/Application/Modules/View-Controllers/QuizManagement/StartQuizViewController.swift
+++ iRevatureQuizPro/Application/Modules/View-Controllers/QuizManagement/StartQuizViewController.swift
@@ -19,7 +19,7 @@ class StartQuizViewController: BaseViewController {
         super.viewDidLoad()
         
         /// - Note: Ran on loading of Page to get all quizzes from API
-        QuizAPI.getAllQuizzes(numberOfRecords: Int.max, finish: {(allQuizzes) in
+        QuizAPI.getAllQuizzes(numberOfRecords: 250, finish: {(allQuizzes) in
             self.allQuizzes = allQuizzes
         })
     }
@@ -31,7 +31,11 @@ class StartQuizViewController: BaseViewController {
     
     @IBAction func submitButton(_ sender: Any) {
         //Checks to make sure that all quizes have been fetched and that there is a matching ID to fetched quizzes
+        print("BUTTON")
         guard !allQuizzes.isEmpty, let quizID = getQuizID() else { return }
+        print("HELLO")
+        print(quizID)
+        AttendeeQuizService.fetchCurrentQuiz(quizId: quizID)
         
         /// - TODO: Write code that gets the quiz by its id from the API. The current implementation of the api calls only fire if the api call is successful. There needs to be a way to have completion handlers run even though it failed. so potentially send and extra Bool variable in the completion or try and send nil values
         
