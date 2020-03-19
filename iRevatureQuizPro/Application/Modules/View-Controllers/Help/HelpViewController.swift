f8d53267f62d7cfe6802f1dc3158b48bdc4b03be
Kjay Shieu <kxs646@case.edu>
2020-03-19 10:09:15 -0400

    readded back button on help screen

--- iRevatureQuizPro/Application/Modules/View-Controllers/Help/HelpViewController.swift
+++ iRevatureQuizPro/Application/Modules/View-Controllers/Help/HelpViewController.swift
@@ -5,33 +5,56 @@
 //  Created by Wesley Otero on 3/13/20.
 //  Copyright © 2020 revature. All rights reserved.
 //
-/*
-    Allows the user to explore the revature website
-*/
 
 import WebKit
 
+//class for a link to the revature website for more information
 class HelpViewController : BaseViewController, WKNavigationDelegate{
     
     var webView: WKWebView!
-    
-    // Setup for loading the WKWeb View
+    //create button with parameters in location and color
+    var backButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 30, width: 100, height: 44))
+        button.addTarget(self, action: #selector(backButtonTapped), for: .touchDown)
+        button.setTitle("Back to app", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+
+    // WKWebView setup
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
         view = webView
     }
     
+    // Displays revature website when view loads
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // URL that the webview will display
         let url = URL(string: "https://www.revature.com/")!
         
-        //Assigns the URL to the webview
+        // Assigns the url to be displaued to the webView
         webView.load(URLRequest(url: url))
         
-        webView.allowsBackForwardNavigationGestures = true
+        webView.addSubview(backButton)
+        myButton()
     }
     
+    //action to go back to app when in Webview
+    @objc func backButtonTapped(){
+        let nextVC = MainStoryBoardViewController.instantiate(fromAppStoryboard: AppStoryboard.Main)
+        nextVC.modalPresentationStyle = .fullScreen
+        present(nextVC,animated: false, completion: nil)
+    }
+    
+    //sets constraints to backButton
+    func myButton() {
+
+    backButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    backButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    backButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: 24).isActive = true
+    backButton.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+    }
 }
